14cd7101d95c27c44245707233a8433489e3fdba
Reski Damayanti <dreski080@gmail.com>
2023-07-01 21:51:48 +0800

    searchNav componen

--- src/components/search-nav-item/SearchNavItem.jsx
+++ src/components/search-nav-item/SearchNavItem.jsx
@@ -1,9 +1,42 @@
 import React from "react";
 
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 
 const SearchNavItem = ({ normalText, boldText, iconImg }) => {
-  return <></>;
+  return <Box sx={{
+    p: {
+      xs: 1.5,
+      md: 2
+    },
+    my: {
+      xs: 1,
+      md: 0
+    },
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '16px'
+  }}>
+    <img src={iconImg} />
+    <Box>
+      <Typography sx={{
+        fontFamily: 'Poppins',
+        fontWeight: '300',
+        fontSize: '14px',
+        color: '#5b5b5b'
+      }}>
+        {normalText}
+      </Typography>
+      <Typography sx={{
+        fontFamily: 'Poppins',
+        fontWeight: '500',
+        fontSize: '16px',
+        color: '#161414'
+      }}>
+        {boldText}
+      </Typography>
+    </Box>
+  </Box>;
 };
 
 export default SearchNavItem;
