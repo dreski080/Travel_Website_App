42fd15162964d97864a3bde440c3434f196ba5ca
Reski Damayanti <dreski080@gmail.com>
2023-07-02 14:24:52 +0800

    destionation desing

--- src/components/destination-item/DestionationItem.jsx
+++ src/components/destination-item/DestionationItem.jsx
@@ -3,7 +3,32 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 
 const DestionationItem = ({ name, location, image }) => {
-  return <></>;
+  return <Box sx={{
+    width: {
+      xs: '70vh',
+      sm: '60vh',
+      md: 'auto'
+    }
+  }}>
+    <img src={image} alt='' style={{ width: '100%', objectFit: 'cover', cursor: 'pointer'}} />
+    <Typography sx={{
+      fontFamily: 'Poppins',
+      fontWeight: '500',
+      fontSize: '16px',
+      color: '#161414'
+    }}>
+      {name}
+    </Typography>
+
+    <Typography sx={{
+      fontFamily: 'Poppins',
+      fontWeight: '300',
+      fontSize: '13px',
+      color: '#979797'
+    }}>
+      {location}
+    </Typography>
+  </Box>;
 };
 
 export default DestionationItem;
