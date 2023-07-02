79e93320d57722ee929b942cd5d323f4c958fc11
Reski Damayanti <dreski080@gmail.com>
2023-07-02 14:57:48 +0800

    hotel & restaurant design

--- src/components/hotel-restaurant-item/HotelRestaurantItem.jsx
+++ src/components/hotel-restaurant-item/HotelRestaurantItem.jsx
@@ -6,7 +6,60 @@ import { Box } from "@mui/system";
 import hotelsLocationIcon from "../../assets/hotels-restaurants/hotels-location-icon.svg";
 
 const HotelRestaurantItem = ({ name, location, image, ratingImage }) => {
-  return <></>;
+  return <Box sx={{display: 'flex', flexDirection: 'column'}}>
+    <Box sx={{
+      width: {
+        xs: '70vh',
+        sm: '60vh',
+        md: 'auto'
+      },
+      display: 'flex',
+      justifyContent: 'center'
+    }}>
+      <img src={image} alt='' style={{ width: '100%', objectFit: 'cover', cursor: 'pointer'}} />
+    </Box>
+    <Typography sx={{
+      mt: 1.5,
+      fontFamily: 'Poppins',
+      fontWeight: '600',
+      fontSize: '16px',
+      color: '#161414',
+      maxWidth: '252px'
+    }}>
+      {name}
+    </Typography>
+    <Box sx={{
+      display: 'flex',
+      flexDirection: {
+        xs: 'column',
+        lg: 'row'
+      },
+      alignItems: {
+        xs: 'flex-start',
+        lg: 'center'
+      },
+      justifyContent: 'space-between',
+      gap: 1,
+      mt: 1.5
+    }}>
+      <Box sx={{
+        display: 'flex',
+        alignItems: 'center',
+        gap: 0.5,
+      }}>
+        <img src={hotelsLocationIcon} alt='' />
+        <Typography sx={{
+           fontFamily: 'Poppins',
+           fontWeight: '400',
+           fontSize: '13px',
+           color: '#979797',
+        }}>
+          {location}
+        </Typography>
+      </Box>
+      <img src={ratingImage} />
+    </Box>
+  </Box>;
 };
 
 export default HotelRestaurantItem;
--- src/components/secondary-button/SecondaryButton.jsx
+++ src/components/secondary-button/SecondaryButton.jsx
@@ -5,7 +5,30 @@ import { Button } from "@mui/material";
 import btnArrowIcon from "../../assets/main/btn-arrow-icon.svg";
 
 const SecondaryButton = ({ text }) => {
-  return <></>;
+  return <Button variant='contained' sx={{
+    width: {
+      xs: '100%',
+      md: 'auto'
+    },
+    backgroundColor: '#f6f4ff',
+    textTransform: 'none',
+    p: 1.25,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 0.5,
+    borderRadius: '12px',
+    fontFamily: 'inherit',
+    fontWeight: '400',
+    fontSize: '16px',
+    color: '#5243c2',
+    '&:hover' : {
+      backgroundColor: '#f6f4ff'
+    }
+  }}>
+    {text}
+    <img src={btnArrowIcon} />
+  </Button>;
 };
 
 export default SecondaryButton;
