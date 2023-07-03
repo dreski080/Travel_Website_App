b0edf6bb3f89bb9d494cbda363171829c806be10
Reski Damayanti <dreski080@gmail.com>
2023-07-03 21:05:33 +0800

    about us & footer

--- src/components/about-us/AboutUs.jsx
+++ src/components/about-us/AboutUs.jsx
@@ -6,7 +6,55 @@ import SecondaryButton from "../secondary-button/SecondaryButton";
 import aboutUsImg from "../../assets/main/about-us-img.svg";
 
 const AboutUs = () => {
-  return <Typography>About Us</Typography>;
+  return <Container disableGutters maxWidth='xl' sx={{
+    px: {
+      xs: 2,
+      sm: 5,
+     md: 10
+    },
+    my: 15
+  }}>
+    <Box sx={{
+      display: 'flex',
+      flexDirection: {
+        xs: 'column',
+        md: 'row'
+      },
+      justifyContent: 'center',
+      alignItems: 'center',
+      gap: 4
+    }}>
+      <Box sx={{flex: 1}}>
+        <Typography sx={{
+          fontFamily: 'Poppins',
+          fontWeight: '600',
+          fontSize: '32px',
+          lineHeight: '48px',
+          color: '#161414',
+          textAlign: {
+            xs: 'center',
+            md: 'left'
+          }
+        }}>About Us</Typography>
+        <Typography sx={{
+          fontFamily: 'Poppins',
+          fontWeight: '400',
+          fontSize: '16px',
+          lineHeight: '32px',
+          color: '#5b5b5b',
+          my: 4
+        }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ullamcorper libero, sed consectetur ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed maximus tempus libero, vel pellentesque urna mollis sed.</Typography>
+        <SecondaryButton text='Read more' />
+      </Box>
+
+      <Box sx={{flex: 1}}>
+        <img src={aboutUsImg} style={{
+          borderRadius: '24px',
+          maxWidth: '100%'
+        }} />
+      </Box>
+    </Box>
+  </Container>;
 };
 
 export default AboutUs;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,7 +12,181 @@ import btnArrowIcon from "../../assets/footer/btn-arrow-icon.svg";
 import inputIcon from "../../assets/footer/input-icon.svg";
 
 const Footer = () => {
-  return <Typography>Footer</Typography>;
+  return <Box sx={{
+    backgroundColor: '#f9f9f9',
+    py: {
+      xs: 10,
+      lg: 15
+    }
+  }}>
+    <Container disableGutters maxWidth= 'xl' sx={{
+      px: {
+        xs: 2,
+        sm: 5,
+        md: 10
+      },
+      display: 'flex',
+      flexDirection: {
+        xs: 'column',
+        lg: 'row'
+      },
+      alignItems: {
+        xs: 'center',
+        lg: 'flex-start'
+      },
+      justifyContent: 'space-between',
+      gap: 8
+    }}>
+      <Box sx={{
+        maxWidth: {
+          xs: 'auto',
+          lg: '355px'
+        },
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: {
+          xs: 'center',
+          lg: 'flex-start'
+        }
+      }}>
+        <img src={logo} />
+        <Typography sx={{
+          mt: 3,
+          mb: 4,
+          fontFamily: 'Poppins',
+          fontWeight: '400',
+          fontSize: '13px',
+          lineHeight: '19.5px',
+          color: '#5b5b5b',
+          textAlign: {
+            xs: 'center',
+            lg: 'left'
+          }
+        }}>
+          We always make our customers happy by providing as many choises as possible
+        </Typography>
+        <Box sx={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: 4
+        }}>
+          <img src={facebookIcon} style={{cursor: 'pointer'}} />
+          <img src={twitterIcon} style={{cursor: 'pointer'}} />
+          <img src={instagramIcon} style={{cursor: 'pointer'}} />
+        </Box>
+      </Box>
+
+      <Box sx={{
+          display: 'flex',
+          width: {
+            xs: '100%',
+            md: 'auto'
+          },
+          gap: {
+            xs: 2,
+            sm: 8
+          },
+          justifyContent: {
+            xs: 'space-between',
+            lg: 'flex-start'
+          }
+        }}>
+          {
+            footerNav.map(item => (
+              <Box key={item.id}>
+                <Typography sx={{
+                  fontFamily: 'Poppins',
+                  fontWeight: '500',
+                  fontSize: {
+                    xs: '14px',
+                    sm: '18px'
+                  },
+                  color: '#161414',
+                  mb: 2.5,
+                }}>
+                  {item.title}
+                </Typography>
+                <Box sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: 1.5,
+                }}>
+                  {
+                    item.links.map(link => (
+                      <Link key={link} href="#" underline="none" color='#5b5b5b' sx={{
+                        width: 'fit-content',
+                        fontWeight: '400',
+                        fontSize: '13px',
+                        '&:hover': {
+                          color: '#161414'
+                        }
+                      }}>
+                      {link}
+                      </Link>
+                    ))
+                  }
+                </Box>
+                </Box>
+            ))
+          }
+        </Box>
+
+      <Box sx={{display: 'flex', flexDirection: 'column', alignItems: {
+        xs: 'center',
+        lg: 'flex-start'
+      },
+      gap: 2.5,
+      maxWidth: {
+        xs: 'auto',
+        lg: '388px'
+      }
+      }}>
+        <Typography sx={{
+          fontFamily: 'Poppins',
+          fontWeight: '500',
+          fontSize: {
+            xs: '14px',
+            sm: '18px'
+          },
+          color: '#161414',
+          textAlign: {
+            xs: 'center',
+            lg: 'left'
+          }
+        }}>
+          Subscribe on our destination review newsletters
+        </Typography>
+        <Box sx={{
+          display: 'flex',
+          flexDirection: {
+            xs: 'column',
+            md: 'row'
+          },
+          alignItems: 'center',
+          gap: 2,
+          position: 'relative'
+        }}>
+          <Box>
+            <img src={inputIcon} style={{ position: 'absolute', left: '16px', top: '36px', transform: 'translateY(-50%)'}} />
+            <input placeholder="Your Email" style={{
+              backgroundColor: '#fff',
+              height: '72px',
+              padding: '16px',
+              border: 'none',
+              outline: 0,
+              borderRadius: '12px',
+              paddingLeft: '54px',
+              fontFamily: 'Poppins',
+              fontWeight: '500',
+              fontSize: '16px',
+              color: '#161414'
+            }} />
+          </Box>
+          <MainButton iconImg={btnArrowIcon} />
+        </Box>
+      </Box>
+    </Container>
+  </Box>;
 };
 
 export default Footer;
